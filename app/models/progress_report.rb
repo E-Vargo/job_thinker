cb80ed5834a91eac8480b7debfd8b79b61a87aa1
E-Vargo <vargoelliott@gmail.com>
2022-03-17 23:03:34 -0700

    validations for user and progress_report

--- app/models/progress_report.rb
+++ app/models/progress_report.rb
@@ -1,4 +1,9 @@
 class ProgressReport < ApplicationRecord
+    #model relationships 
     belongs_to :user 
     belongs_to :career
+
+    #validations 
+    validates :content, length: {in: 150..3500}
+    validates :date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
-         
+#model realtionships       
   has_many :plans 
   has_many :commitments
   has_many :progress_reports
   has_many :careers, through: :progress_reports 
 
+#validations
+validates :name, length: {minimum: 3}
+validates :email, uniqueness: true 
 
+#Devise
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[github]
@@ -15,11 +19,7 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name   # assuming the user model has a name
-      #user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+      user.name = auth.info.name
     end
   end
 end
